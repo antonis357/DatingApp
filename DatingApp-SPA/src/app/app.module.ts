1b1e6e725082ea61ee0451970539bcd22e6427ea
Antonis Patergianakis <antonis_patergianakis@hotmail.gr>
2019-06-09 21:33:20 +0300

    End of section 7

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./nav/nav.component";
@@ -12,18 +13,43 @@ import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptor } from "./_services/error.interceptor";
 import { AlertifyService } from './_services/alertify.service';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BsDropdownModule.forRoot(),BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-  providers: [AuthService, 
-    AlertifyService,
-    //ErrorInterceptorProvider,
-   {
-     provide: HTTP_INTERCEPTORS,
-     useClass: ErrorInterceptor,
-     multi: true
-   },],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
+   ],
+   imports: [
+     BrowserModule,
+     AppRoutingModule,
+     HttpClientModule,
+     FormsModule,
+     BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
+   ],
+   providers: [
+      AuthService,
+      AlertifyService,
+      AuthGuard,
+      //ErrorInterceptorProvider,
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: ErrorInterceptor,
+        multi: true
+      },
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {}
