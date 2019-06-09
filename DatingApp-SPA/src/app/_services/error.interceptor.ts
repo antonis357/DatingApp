5940714a5d0055f53193f05a04bd22cc46915a8e
Antonis Patergianakis <antonis_patergianakis@hotmail.gr>
2019-06-09 18:35:35 +0300

    End of section 6

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -12,30 +12,32 @@ import { catchError } from "rxjs/operators";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  intercept( 
+  intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError(error => {
         if (error instanceof HttpErrorResponse) {
-          if(error.status === 401)
-          return throwError(error.statusText);
+          if (error.status === 401) return throwError(error.statusText);
+          if (error.status === 400) return throwError('Username already exists!'); // fix username exists
           const applicationError = error.headers.get("Application-Error");
           if (applicationError) {
             console.error(applicationError);
             return throwError(applicationError);
           }
           const serverError = error.error.errors;
-          let modalStateErrors = '';
-          if(serverError && typeof serverError === 'object') {
+          let modalStateErrors = "";
+          if (serverError && typeof serverError === "object") {
             for (const key in serverError) {
-              if(serverError[key]) {
-                modalStateErrors += serverError[key] + '\n';
+              if (serverError[key]) {
+                modalStateErrors += serverError[key] + "\n";
               }
             }
           }
-          return throwError(modalStateErrors || serverError || 'Undefined Server Error');
+          return throwError(
+            modalStateErrors || serverError || "Undefined Server Error"
+          );
         }
       })
     );
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./nav/nav.component";
@@ -10,11 +11,14 @@ import { AuthService } from "./_services/auth.service";
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptor } from "./_services/error.interceptor";
+import { AlertifyService } from './_services/alertify.service';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-  providers: [AuthService, //ErrorInterceptorProvider,
+  imports: [BsDropdownModule.forRoot(),BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [AuthService, 
+    AlertifyService,
+    //ErrorInterceptorProvider,
    {
      provide: HTTP_INTERCEPTORS,
      useClass: ErrorInterceptor,
