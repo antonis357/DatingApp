b26b10f4f49d7fd089af60ffcf834a88883fdb3a
antonis <antonis.cyberlogic.gr@outlook.com>
2019-07-09 13:11:54 +0300

    End of Section 8

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
@@ -11,32 +12,35 @@ namespace DatingApp.API.Data
         public DatingRepository(DataContext context)
         {
             _context = context;
-
         }
         
         public void Add<T>(T entity) where T : class
         {
-            throw new System.NotImplementedException();
+            _context.Add(entity);
         }
 
         public void Delete<T>(T entity) where T : class
         {
-            throw new System.NotImplementedException();
+            _context.Remove(entity);
         }
 
-        public Task<User> GetUser()
+        public async Task<User> GetUser(int id)
         {
-            throw new System.NotImplementedException();
+            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+
+            return user;
         }
 
-        public Task<IEnumerable<User>> GetUsers()
+        public async Task<IEnumerable<User>> GetUsers()
         {
-            throw new System.NotImplementedException();
+            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+
+            return users;
         }
 
-        public Task<bool> SaveAll()
+        public async Task<bool> SaveAll()
         {
-            throw new System.NotImplementedException();
+            return await _context.SaveChangesAsync() > 0;
         }
     }
 }
